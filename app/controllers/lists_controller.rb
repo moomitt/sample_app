d07cb1c5e292270f8ba1db01b9f0299dbe0799df
EC2 Default User <ec2-user@ip-172-31-50-62.ec2.internal>
2022-05-05 05:23:00 +0000

    [Start] 4-5

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -1,25 +1,33 @@
 class ListsController < ApplicationController
   def new
-    @list = List.new    # Viewへ渡すインスタンス変数@list ← 空のModelオブジェクトList.newを生成
-  end
+    @list = List.new    　　#インスタンス(空のModel)Listを作成、インスタンス変数@listに代入
+    end
 
   def create
-    list = List.new(list_params)   # データを受け取り、新規登録するためのインスタンス作成
-    list.save                      # データをデータベースに保存するためのsaveメソッド実行
-    redirect_to '/top'             # トップ画面へリダイレクト
+    list = List.new(list_params)     # インスタンス(モデル)をlist_paramsの内容で作成、ローカル変数listに代入　※Viewに渡さないので、ローカル変数
+    list.save                        # ローカル変数listをデータベースに保存
+    redirect_to list_path(list.id)   # list_path(=showアクション)へリダイレクト
   end
     
   def index
-    @lists = List.all      # インスタンス変数@lists ← listsテーブルに保存されている全レコードを取得
+    @lists = List.all     　　　　　 # listsテーブル内の全レコードを取得、インスタンス変数@listsに代入
   end
 
   def show
-  end
+    @list = List.find(params[:id])     # listsテーブル内の:id番目のレコードを取得、インスタンス変数@listsに代入
+  end                                  # params[:id] ... urlからid番号(:id)を取得
 
   def edit
+    @list = List.find(params[:id])     # listsテーブル内の:id番目のレコードを取得、インスタンス変数@listsに代入
+  end                                  # params[:id] ... urlからid番号(:id)を取得
+  
+  def update
+    list = List.find(params[:id])      # listsテーブル内のn番目のレコードを取得、ローカル変数listに代入　　※Viewに渡さないので、ローカル変数
+    list.update(list_params)           # ローカル変数list(=listsテーブル内のn番目のレコード)を、list_paramsの内容で更新
+    redirect_to list_path(list.id)     # list_path(=showアクション)へリダイレクト
   end
   
-  private      # ストロングパラメータ
+  private      　　　　　　　　　　　　# ストロングパラメータ：フォーム入力内容を安全に保存
   def list_params
     params.require(:list).permit(:title, :body)
   end
