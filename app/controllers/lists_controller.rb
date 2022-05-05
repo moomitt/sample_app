6e09ff2fb3ead5e5619c9f9cc2e60b3218c3af1f
EC2 Default User <ec2-user@ip-172-31-50-62.ec2.internal>
2022-05-05 09:22:21 +0000

    [Start] 4-9

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -1,35 +1,44 @@
 class ListsController < ApplicationController
   def new
-    @list = List.new    　　#インスタンス(空のModel)Listを作成、インスタンス変数@listに代入
-    end
+    @list = List.new                #インスタンス(空のModel)Listを作成、インスタンス変数@listに代入
+  end
 
   def create
-    list = List.new(list_params)     # インスタンス(モデル)をlist_paramsの内容で作成、ローカル変数listに代入　※Viewに渡さないので、ローカル変数
-    list.save                        # ローカル変数listをデータベースに保存
-    redirect_to list_path(list.id)   # list_path(=showアクション)へリダイレクト
+    @list = List.new(list_params)      # インスタンス(モデル)をlist_paramsの内容で作成、ローカル変数listに代入
+    if @list.save                      # インスタンス変数listをデータベースに保存
+      redirect_to list_path(list.id)   # list_path(=showアクション)へリダイレクト
+    else
+      render :new
+    end
   end
     
   def index
-    @lists = List.all     　　　　　 # listsテーブル内の全レコードを取得、インスタンス変数@listsに代入
+    @lists = List.all                # listsテーブル内の全レコードを取得、インスタンス変数@listsに代入
   end
 
   def show
-    @list = List.find(params[:id])     # listsテーブル内の:id番目のレコードを取得、インスタンス変数@listsに代入
-  end                                  # params[:id] ... urlからid番号(:id)を取得
+    @list = List.find(params[:id])   # listsテーブル内の:id番目のレコードを取得、インスタンス変数@listsに代入
+  end                                # params[:id] ... urlからid番号(:id)を取得
 
   def edit
-    @list = List.find(params[:id])     # listsテーブル内の:id番目のレコードを取得、インスタンス変数@listsに代入
-  end                                  # params[:id] ... urlからid番号(:id)を取得
+    @list = List.find(params[:id])   # listsテーブル内の:id番目のレコードを取得、インスタンス変数@listsに代入
+  end                                # params[:id] ... urlからid番号(:id)を取得
   
   def update
-    list = List.find(params[:id])      # listsテーブル内のn番目のレコードを取得、ローカル変数listに代入　　※Viewに渡さないので、ローカル変数
-    list.update(list_params)           # ローカル変数list(=listsテーブル内のn番目のレコード)を、list_paramsの内容で更新
-    redirect_to list_path(list.id)     # list_path(=showアクション)へリダイレクト
+    list = List.find(params[:id])    # listsテーブル内のn番目のレコードを取得、ローカル変数listに代入　　※Viewに渡さないので、ローカル変数
+    list.update(list_params)         # ローカル変数list(=listsテーブル内のn番目のレコード)を、list_paramsの内容で更新
+    redirect_to list_path(list.id)   # list_path(=showアクション)へリダイレクト
   end
   
-  private      　　　　　　　　　　　　# ストロングパラメータ：フォーム入力内容を安全に保存
+  def destroy
+    list = List.find(params[:id])    # レコードを１件取得
+    list.destroy                     # レコードを削除
+    redirect_to '/lists'             # 投稿一覧画面へリダイレクト
+  end
+  
+  private
   def list_params
-    params.require(:list).permit(:title, :body)
+    params.require(:list).permit(:title, :body, :image)
   end
   
 end
--- app/models/list.rb
+++ app/models/list.rb
@@ -1,2 +1,7 @@
 class List < ApplicationRecord
+  has_one_attached :image         #ActiveStorageの使用を宣言（:imageカラムが追加されたかのように扱える）
+
+  validates :title, presence: true    #対象データ:titleの存在:true（存在しなければならない）
+  validates :body, presence: true     #対象データ:bodyの存在:true（存在しなければならない）
+  validates :image, presence: true    #対象データ:imageの存在:true（存在しなければならない）
 end
